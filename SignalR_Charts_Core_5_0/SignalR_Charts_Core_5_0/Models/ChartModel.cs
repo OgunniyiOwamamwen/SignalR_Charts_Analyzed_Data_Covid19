1885452b38a1ab7096298e5755b3cbd0b76ed56b
Ogunniyi Owamamwen.F <ogunniyiowamamwen@gmail.com>
2020-12-16 13:49:09 +0100

    Add files via upload

---
+++ SignalR_Charts_Core_5_0/SignalR_Charts_Core_5_0/Models/ChartModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SignalR_Charts_Core_5_0.Models
+{
+    public class ChartModel
+    {
+        public List<int> Data { get; set; }
+        public string Label { get; set; }
+
+        public ChartModel()
+        {
+            Data = new List<int>();
+        }
+    }
+}
---
+++ SignalR_Charts_Core_5_0/SignalR_Charts_Core_5_0/TimerFeature/TimerManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace SignalR_Charts_Core_5_0.TimerFeature
+{
+    public class TimerManager
+    {
+        private Timer _timer;
+        private AutoResetEvent _autoResetEvent;
+        private Action _action;
+
+        public DateTime TimerStarted { get; set; }
+
+
+        public TimerManager( Action action)
+        {
+            _action = action;
+            _autoResetEvent = new AutoResetEvent(false);
+            _timer = new Timer(Execute, _autoResetEvent, 1000, 2000);
+            TimerStarted = DateTime.Now;
+        }
+
+        // Add  
+        public void Execute(object stateInfo)
+        {
+            _action();
+
+            if((DateTime.Now - TimerStarted).Seconds > 60)
+            {
+                // Destructor
+                _timer.Dispose();
+            }
+        }
+
+    }
+}
