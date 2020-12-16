cbf99bc195991580e84eb4ebffbc9d839701afab
Ogunniyi Owamamwen.F <ogunniyiowamamwen@gmail.com>
2020-12-16 13:53:23 +0100

    Add files via upload

---
+++ SignalR_Charts_Core_5_0/SignalR_Charts_Core_5_0/DataStorage/DataManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using SignalR_Charts_Core_5_0.Models;
+
+namespace SignalR_Charts_Core_5_0.DataStorage
+{
+    public class DataManager
+    {
+        public static List<ChartModel> GetData()
+        {
+            var rc = new Random();
+            return new List<ChartModel>
+            {
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Milano" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50) }, Label = "Bergamo" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Brescia"} ,
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Monza" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Cremona" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Mantova" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Pavia" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Lecco" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Sondrio" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Como" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Lodi" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Monza" },
+                new ChartModel {Data = new List<int> { rc.Next(1, 50)}, Label = "Varese" }
+            };
+        }
+        public DataManager()
+        {
+        }
+    }
+}
---
+++ SignalR_Charts_Core_5_0/SignalR_Charts_Core_5_0/HubConfig/ChartHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SignalR_Charts_Core_5_0.Models;
+
+namespace SignalR_Charts_Core_5_0.HubConfig
+{
+    public class ChartHub : Hub
+    {
+        // you can comment all the code here
+        // When you click on the frontend UI it we broadCast to any current UI user
+        public async Task BroadcastChartData(List<ChartModel> data) =>
+            await Clients.All.SendAsync("broadcastchartdata", data);
+
+
+        public ChartHub()
+        {
+        }
+    }
+}
